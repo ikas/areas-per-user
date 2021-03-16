e5eab7cc6d9ae0d368cdc5d74a210f8186f5b524
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2021-03-16 14:35:51 +0000

    Setup Mongo connection and count total number of AoIs

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world!');
+const MongoClient = require('mongodb').MongoClient;
+
+const main = async () => {
+    let client;
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+        const countAreas = await client.db('area').collection('areas').countDocuments();
+        console.log(`Total AoI count: ${countAreas}`);
+    } catch (err) {
+        console.err(err);
+    } finally {
+        if (client) {
+            await client.close();
+        }
+    }
+}
+
+main()
+    .then(() => { console.log('script ended'); })
+    .catch(err => { console.error(err); });
